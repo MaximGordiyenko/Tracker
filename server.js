2408ca8a42408e1649a481b6d98040da0406127a
Maxim <maxim.gordiyenko@gmail.com>
2020-03-29 23:45:29 +0300

    add login form

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
+app.use(express.static('public')); // relative path of client-side code
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: __dirname});
+});
 
-app.listen(8888, () => console.log('May node be with you on port 8888'));
+app.listen(4000, () => console.log('May node be with you on port 4000'));
