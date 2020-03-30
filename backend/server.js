26790ce6f3855915ef63089e2e680ef4d68bcb70
Maxim <maxim.gordiyenko@gmail.com>
2020-03-30 15:26:44 +0300

    added mongoDB and config file

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const PORT = process.env.NODE_PORT;
+const DB = process.env.MONGO_PATH;
 const cors = require('cors');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
 app.use(express.static('frontend/public'));
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: __dirname});
+
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then((result) => {
+    console.log("MongoDB database connection established successfully:");
+    console.log(result.connections[0].host);
+    console.log(result.connections[0].name);
+    console.log(result.connections[0].name);
+    console.log(result.connections[0].user);
+    console.log(result.connections[0].pass);
+    console.log(result.models.User);
+    console.log(result.models.Todo);
 });
 
-app.listen(4000, () => console.log('May node be with you on port 4000'));
+app.listen(PORT, () => {
+    console.debug("Server is running on Port: " + PORT);
+});
