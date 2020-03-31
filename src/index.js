7277473374633357912a4dd716456955960a4aca
SirSerje <sir.serje@gmail.com>
2020-03-31 10:42:30 +0300

    add esm

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const dotenv = require('dotenv');
+import dotenv from 'dotenv'; // now imports are allowed TODO: update all other imports
 const app = express();
 
 const envConfig = dotenv.config();
